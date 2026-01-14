fd3692004cd6cc4924c0aecbe60acd4389470517
J <yeji02j@gmail.com>
2026-01-14 21:35:26 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd2 test222
+          Learn GithubAction cicd3
         </a>
       </header>
     </div>
@@ -23,6 +23,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
